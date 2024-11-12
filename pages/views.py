1dc6b6356ffdc4ea69996998e8f4902fd73f84b9
ayushi <ayushi>
2024-11-12 14:10:23 +0000

    changes in html

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page_view(request):
     return HttpResponse("Hello, World!")
+def about_page_view(request):
+    return render(request,'about.html')
  
